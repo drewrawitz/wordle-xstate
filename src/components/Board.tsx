1143385752a097e4526c006d1bf4e6ab8941cef4
Drew Rawitz <email@drewrawitz.com>
2022-03-03 13:16:41 -0500

    adding status colors to the board tiles

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,22 +1,38 @@
+import classnames from "classnames";
 import { wordleMachine } from "../machines/wordle";
 import { useMachine } from "@xstate/react";
+import { letterStatus } from "../utils/wordle";
 
 interface RowsProps {
   guess?: string;
+  answer: string;
 }
 
-const Rows: React.FC<RowsProps> = ({ guess }) => {
+const Rows: React.FC<RowsProps> = ({ guess, answer }) => {
   const ROWS = 5;
   const rowKeys = Array(ROWS).keys();
   const guessArray = guess?.split("") || [];
 
+  const getLetterStatus = (letter: string, index?: number) =>
+    letter ? letterStatus(letter, answer, guessArray, index) : "unplayed";
+
   return (
     <div className="Row-wrapper">
-      {Array.from(rowKeys).map((i, idx) => (
-        <div key={i} className="Row-tile">
-          {guessArray[idx]}
-        </div>
-      ))}
+      {Array.from(rowKeys).map((i, idx) => {
+        const letter = guessArray[idx] || "";
+        const status = getLetterStatus(letter, idx);
+
+        return (
+          <div
+            key={i}
+            className={classnames("Row-tile", {
+              [`Row-tile--${status}`]: status !== "unplayed",
+            })}
+          >
+            {letter}
+          </div>
+        );
+      })}
     </div>
   );
 };
@@ -25,7 +41,7 @@ export default function Board() {
   const COLUMNS = 6;
   const columnKeys = Array(COLUMNS).keys();
   const [state] = useMachine(wordleMachine);
-  const { guesses } = state.context;
+  const { guesses, answer } = state.context;
 
   return (
     <div className="Board">
@@ -34,7 +50,7 @@ export default function Board() {
 
         return (
           <div key={i} className="Column">
-            <Rows guess={guess} />
+            <Rows guess={guess} answer={answer} />
           </div>
         );
       })}
